b1f8ffd905ae681095eeb21e4b4596256ba06b82
Ilkka Hakkarainen <ilkka.hakkarainen@cs.helsinki.fi>
2015-03-14 16:19:27 +0200

    Add commenting to version function

--- lib/squire/version.sh
+++ lib/squire/version.sh
@@ -1,5 +1,7 @@
+# The authoritative version number of this library.
 SQUIRE_VERSION='0.0.1'
 
+# Print out the library name and version to stdout.
 function version() {
   echo "${SQUIRE_APPLICATION_NAME} ${SQUIRE_VERSION}"
 }
